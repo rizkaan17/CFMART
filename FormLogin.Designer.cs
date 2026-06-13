bb955e3cf2e20dd9d0c6bceb3f24e0483c2c1ad7
rosarsydh <ekarosa287@gmail.com>
2026-06-13 18:55:59 +0700

    Add files via upload

---
+++ FormLogin.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace CFMART.Views
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
+            panel1 = new Panel();
+            btnLogin = new Button();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            tbPassword = new TextBox();
+            tbUsername = new TextBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.SlateGray;
+            panel1.Controls.Add(btnLogin);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(tbPassword);
+            panel1.Controls.Add(tbUsername);
+            panel1.Location = new Point(994, 477);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(998, 813);
+            panel1.TabIndex = 0;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.SeaGreen;
+            btnLogin.FlatStyle = FlatStyle.Popup;
+            btnLogin.Font = new Font("Dubai Medium", 8.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnLogin.ForeColor = SystemColors.ButtonHighlight;
+            btnLogin.Location = new Point(338, 655);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(313, 86);
+            btnLogin.TabIndex = 7;
+            btnLogin.Text = "LOGIN";
+            btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Dubai", 10.875F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ButtonHighlight;
+            label4.Location = new Point(44, 483);
+            label4.Name = "label4";
+            label4.Size = new Size(147, 49);
+            label4.TabIndex = 6;
+            label4.Text = "Password :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Dubai", 10.875F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ButtonHighlight;
+            label3.Location = new Point(44, 341);
+            label3.Name = "label3";
+            label3.Size = new Size(153, 49);
+            label3.TabIndex = 5;
+            label3.Text = "Username :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Dubai", 10.875F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ActiveCaptionText;
+            label2.Location = new Point(300, 248);
+            label2.Name = "label2";
+            label2.Size = new Size(387, 49);
+            label2.TabIndex = 4;
+            label2.Text = "Masuk untuk mengelola sistem";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Dubai", 10.875F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(406, 199);
+            label1.Name = "label1";
+            label1.Size = new Size(181, 49);
+            label1.TabIndex = 3;
+            label1.Text = "KARYAWAN";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Location = new Point(422, 59);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(150, 126);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // tbPassword
+            // 
+            tbPassword.Location = new Point(44, 535);
+            tbPassword.Name = "tbPassword";
+            tbPassword.Size = new Size(870, 39);
+            tbPassword.TabIndex = 1;
+            // 
+            // tbUsername
+            // 
+            tbUsername.Location = new Point(44, 406);
+            tbUsername.Name = "tbUsername";
+            tbUsername.Size = new Size(870, 39);
+            tbUsername.TabIndex = 0;
+            // 
+            // FormLogin
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(2139, 987);
+            Controls.Add(panel1);
+            Name = "FormLogin";
+            Text = "FormLogin";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private TextBox tbPassword;
+        private TextBox tbUsername;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Button btnLogin;
+    }
+}
